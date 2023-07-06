db4609403ef5d0fb564abc320fb81f83e44daac3
The Real Mi <miaolmedillo@gmail.com>
2023-07-06 17:52:50 -0400

    added for loop to generate password

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ var special = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
 function generatePassword() {
     var newPassword = ""
+    var passCharacters = ""
     //Prompt 1 ask for password length. (length MUST be between 8 and 128 characters)
     var passLength = window.prompt("Please choose a password length between 8 and 128 characters")
 
@@ -24,32 +25,45 @@ function generatePassword() {
         //Prompt 5 confirm special character
         var isSpecial = window.confirm("Would you like to include a special character in your password?")
 
+        // Concat lowercase character to new password
         if (isLowercase === true) {
-            var randomIndex = Math.floor(Math.random() * lowercase.length)
-            newPassword = newPassword + lowercase[randomIndex]
+            passCharacters = passCharacters + lowercase
         }
+        // Concat uppercase character to new password
         if (isUppercase === true) {
-            var randomIndex = Math.floor(Math.random() * uppercase.length)
-            newPassword = newPassword + uppercase[randomIndex]
+            passCharacters = passCharacters + uppercase
         }
+        // Concat number to new password
         if (isNumbers === true) {
-            var randomIndex = Math.floor(Math.random() * numbers.length)
-            newPassword = newPassword + numbers[randomIndex]
+            passCharacters = passCharacters + numbers
         }
+        // Concat special character to new password
         if (isSpecial === true) {
-            var randomIndex = Math.floor(Math.random() * special.length)
-            newPassword = newPassword + special[randomIndex]
+            passCharacters = passCharacters + special
         }
+
+        // Generate new password. 
+        // Add new character at random to password until password reaches specified "passLength"
+        for (i = 0; newPassword.length < passLength; i++) {
+            var randomIndex = Math.floor(Math.random() * passCharacters.length)
+            newPassword = newPassword + passCharacters[randomIndex]
+        }
+
+
+        // Verify character type is chosen
         if (isLowercase === false && isUppercase === false && isNumbers === false && isSpecial === false) {
             window.alert("You must pick at least one character type")
             return ""
         }
     }
 
+    // Verify password length
     else {
         window.alert("Please input a valid password length between 8 and 128")
     }
+    return newPassword
 }
+
 
 // Write password to the #password input
 function writePassword() {
